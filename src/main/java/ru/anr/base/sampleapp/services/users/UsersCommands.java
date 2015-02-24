c2f957e6f4223363104f5f782d9f665a28540907
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2015-02-24 13:32:01 +0500

    Added a new ACL/Role security model

--- src/main/java/ru/anr/base/sampleapp/services/users/UsersCommands.java
+++ src/main/java/ru/anr/base/sampleapp/services/users/UsersCommands.java
@@ -19,7 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -65,7 +65,7 @@ public class UsersCommands extends AbstractApiCommandStrategyImpl {
      * @return response
      */
     @Override
-    @Secured({ "ROLE_USER" })
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseModel get(APICommand cmd) {
 
         UserModelResponse m = new UserModelResponse();
@@ -81,7 +81,7 @@ public class UsersCommands extends AbstractApiCommandStrategyImpl {
      * {@inheritDoc}
      */
     @Override
-    @Secured({ "ROLE_USER" })
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseModel put(APICommand cmd) {
 
         UserModel m = cmd.getRequest();
